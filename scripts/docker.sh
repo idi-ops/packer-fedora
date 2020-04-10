11e83b16c54f1e9c132c62a03edd7a300aa3a380
Giovanni Tirloni <giovanni.tirloni@gmail.com>
2020-04-10 17:12:47 -0300

    INFRA-235: Add Fedora 31

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -eux
 
 dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-dnf install -y docker-ce
+dnf install -y docker-ce grubby
 systemctl enable docker
 groupadd docker
 usermod -a -G docker vagrant
+
+grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
